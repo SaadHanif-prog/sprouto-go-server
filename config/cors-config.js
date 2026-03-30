4f425cca616ea5f92d2d9cac2693ccad3314bb6f
M Saad Hanif <support@sprouto.com>
2026-03-30 18:44:01 +0500

    change cors policy

--- config/cors-config.js
+++ config/cors-config.js
@@ -3,7 +3,7 @@ module.exports = getCorsOptions = () => {
     origin: (origin, callback) => {
       const allowedOrigins = [
         "http://localhost:5173",
-        "https://sproutogo-client.vercel.app"
+        "https://sprouto-go-client-1.onrender.com"
       ];
 
       if (!origin || allowedOrigins.includes(origin)) {
